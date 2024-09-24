57e9a4e4f3a04e70142855dcb8994242264c35f3
VitaliyMal <161741522+VitaliyMal@users.noreply.github.com>
2024-09-24 14:17:05 +0300

    upd IngredientVM

--- Workshop.App/ViewModels/IngredientViewModel.cs
+++ Workshop.App/ViewModels/IngredientViewModel.cs
@@ -1,30 +1,74 @@
 ﻿using System.Collections.ObjectModel;
 using Workshop.App.Core;
-using Workshop.Core.Entity;
+//using Workshop.Core.Entity;
 using Workshop.Core.Service;
 
 namespace Workshop.App.ViewModels
 {
     public class IngredientViewModel : ObservableObject
     {
-        private string _input = string.Empty;
-        public string Input
+        private string _title = string.Empty;
+        public string Title
         {
-            get => _input;
+            get => _title;
             set
             {
-                _input = value;
-                OnPropertyChanged("Input");
+                _title = value;
+                OnPropertyChanged("Title");
             }
         }
 
-        private ObservableCollection<Ingredient> _ingredientList = new ObservableCollection<Ingredient>();
-        public ObservableCollection<Ingredient> IngredientList { get => _ingredientList; set { _ingredientList = value; OnPropertyChanged("IngredientList"); } }
+        private int _amount = 0;
+        public int Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged("Amount");
+            }
+        }
+
+        private int _minimalAmount = 0;
+        public int MinimalAmount
+        {
+            get => _minimalAmount;
+            set
+            {
+                _minimalAmount = value;
+                OnPropertyChanged("MinimalAmount");
+            }
+        }
+
+        private int _cost = 0;
+        public int Cost
+        {
+            get => _cost;
+            set
+            {
+                _cost = value;
+                OnPropertyChanged("Cost");
+            }
+        }
+
+        private int _ingredientType_id = 0; // нужен ли он ???????????????????????????????????????????????????
+        public int IngredientType_id
+        {
+            get => _ingredientType_id;
+            set
+            {
+                _ingredientType_id = value;
+                OnPropertyChanged("IngredientType_id");
+            }
+        }
+
+        private ObservableCollection<IngredientDTO> _ingredientList = new ObservableCollection<IngredientDTO>();
+        public ObservableCollection<IngredientDTO> IngredientList { get => _ingredientList; set { _ingredientList = value; OnPropertyChanged("IngredientList"); } }
 
         private IngredientService ingredientService;
 
-        private Ingredient _selectedIngredient;
-        public Ingredient SelectedIngredient
+        private IngredientDTO _selectedIngredient;
+        public IngredientDTO SelectedIngredient
         {
             get => _selectedIngredient;
             set
@@ -37,59 +81,87 @@ namespace Workshop.App.ViewModels
         public IngredientViewModel(IngredientService service)
         {
             ingredientService = service;
-            IngredientList = new ObservableCollection<Ingredient>(ingredientService.GetAll());
+            Task.Run(() => Fetch());
         }
 
-        private RelayCommand addCommand;
-        public RelayCommand AddCommand
+        public async Task Fetch()
+        {
+            IngredientList = new ObservableCollection<IngredientDTO>(await ingredientService.GetAll());
+        }
+
+        private AsyncRelayCommand addCommand;
+        public AsyncRelayCommand AddCommand
         {
             get
             {
-                return addCommand ??
-                  (addCommand = new RelayCommand(obj =>
-                  {
-                      ingredientService.Create(
-                          new Ingredient(Input)
-                          );
-                      IngredientList = new ObservableCollection<Ingredient>(ingredientService.GetAll());
-                  }));
+                return addCommand ?? (
+                    addCommand = new AsyncRelayCommand(() => Task.Run(
+                          async () =>
+                          {
+                              try
+                              {
+                                  await ingredientService.Create(
+                                      new IngredientDTO(0, Title, Amount, MinimalAmount, Cost, IngredientType_id)
+                                      );
+                                  await Fetch();
+                              }
+                              catch (Exception ex)
+                              {
+                                  MessageBox.Show(ex.Message);
+                                  //throw(ex);                                  
+                              }
+                          }))
+                    );
             }
         }
 
-        private RelayCommand deleteCommand;
-        public RelayCommand DeleteCommand
+        private AsyncRelayCommand deleteCommand;
+        public AsyncRelayCommand DeleteCommand
         {
             get
             {
-                return deleteCommand ??
-                  (deleteCommand = new RelayCommand(obj =>
-                  {
-                      ingredientService.Delete(
-                          SelectedIngredient.Id
-                          );
-                      IngredientList = new ObservableCollection<Ingredient>(ingredientService.GetAll());
-                  }));
+                return deleteCommand ?? (
+                    deleteCommand = new AsyncRelayCommand(() => Task.Run(
+                        async () =>
+                        {
+                            await ingredientService.Delete(
+                                SelectedIngredient.Id
+                                    );
+                            await Fetch();
+                        }))
+                    );
             }
         }
 
-        private RelayCommand editCommand;
-        public RelayCommand EditCommand
+        private AsyncRelayCommand editCommand;
+        public AsyncRelayCommand EditCommand
         {
             get
             {
                 return editCommand ??
-                  (editCommand = new RelayCommand(obj =>
-                  {
-                      SelectedIngredient.Title = Input;
-                      SelectedIngredient.Amount = Convert.ToInt16(Input);
-                      SelectedIngredient.MinimalAmount = Convert.ToInt16(Input);
-                      SelectedIngredient.Cost = Convert.ToInt16(Input);
-                      //SelectedIngredient.Type = Input;
-                      ingredientService.Update(
-                          SelectedIngredient
-                          );
-                      IngredientList = new ObservableCollection<Ingredient>(ingredientService.GetAll());
-                  }));
+                  (editCommand = new AsyncRelayCommand(() => Task.Run(
+                      async () =>
+                      {
+                          try
+                          {
+                              await ingredientService.Update(
+                                new UpdateIngredientDTO(
+                                    SelectedIngredient.Id,
+                                    Title,
+                                    Amount,
+                                    MinimalAmount,
+                                    Cost,
+                                    IngredientType_id
+                                    )
+                                );
+                              await Fetch();
+                          }
+                          catch (Exception ex)
+                          {
+                              ////////////////////// логика когда срабатывает                              
+                          }
+                      }))
+                  );
             }
         }
     }
